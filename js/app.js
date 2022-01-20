7fca06d7d87cccb3e97d51471d616c0cb2c0fca9
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-01-20 15:25:11 +0300

    create shopping cart count

--- js/app.js
+++ js/app.js
@@ -24,3 +24,19 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+///shoping cart count
+$(document).ready(function(){
+  //create variable
+  var counts = 0;
+  $(".addtocart").click(function () {
+  //to number and increase to 1 on each click
+     counts += +1;
+     $(".cart-counter").animate({
+  //show span with number
+               opacity: 1
+           }, 300, function () {
+  //write number of counts into span
+               $(this).text(counts);
+           });
+       }); 
+});
