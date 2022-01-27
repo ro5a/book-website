a10a671ad9297602240d0b9ee38824ae1cd510c7
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-01-27 09:13:01 +0300

    add timer

--- js/app.js
+++ js/app.js
@@ -64,3 +64,25 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+// timer of product display
+var productCount = 400;
+
+function countdown(){
+  
+  day = 28;
+  today           = new Date();
+  BigDay          = new Date("January "+day+",2022");
+
+  msPerDay        = 24 * 60 * 60 * 1000;
+
+  timeLeft        = (BigDay.getTime() - today.getTime());
+
+  e_daysLeft      = timeLeft / msPerDay;
+  daysLeft        = Math.floor(e_daysLeft);
+  e_hrsLeft       = e_daysLeft*24;
+  hrsLeft         = Math.floor(e_hrsLeft);
+  minsLeft        = Math.floor((e_hrsLeft - hrsLeft)*60);
+  document.getElementById('output').innerHTML = "<p>There are only<b> " + daysLeft + " days " + hrsLeft +" hours and " + minsLeft + " minutes left </b> Until January 30th 2022</p>";
+}
+countdown()
+window.setInterval(function(){ countdown(); productCount -=1; },60000)
